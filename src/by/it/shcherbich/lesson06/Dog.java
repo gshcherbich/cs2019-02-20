ca10076f65b36c9d3353ed5f2083f72ad038a92c
gshcherbich <grifryhor@gmail.com>
2019-03-15 15:01:15 +0300

    lesson05 Task C3

---
+++ src/by/it/shcherbich/lesson06/Dog.java
@@ -0,0 +1,36 @@
+package by.it.shcherbich.lesson06;
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age>=0)
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return ("Кличка: " +name+". Возраст: " + age);
+    }
+}
---
+++ src/by/it/shcherbich/lesson06/DogHelper.java
@@ -0,0 +1,20 @@
+package by.it.shcherbich.lesson06;
+
+public class DogHelper {
+   static void printAllNames(Dog[] dogs){
+       for (Dog dog : dogs) {
+           System.out.print(dog.getName()+" ");
+
+       }
+    }
+    static double averageAge(Dog[] dogs){
+
+       double sum=0;
+        for (Dog dog : dogs) {
+            sum=sum+dog.getAge();
+        }
+       return sum/dogs.length;
+
+
+    }
+}
